4ef722aec9060b82fddd49ba2103a92c815b0dc3
jagerbomb092 <51813727+jagerbomb092@users.noreply.github.com>
2020-11-16 22:25:57 +0700

    Add files via upload

---
+++ hkey.js
@@ -0,0 +1,16 @@
+const game = ()=>{
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = () =>{
+        const playBtn = document.querySelector(".btn-play");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
+
+        playBtn.addEventListener("click", () => {
+            introScreen.classList.add("fadeOut");
+
+        });
+    };
+    startGame();
+};
